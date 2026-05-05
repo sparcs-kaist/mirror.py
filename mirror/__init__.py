5fafd0990613f76302cfbb9a47c453f940edaedd
DoyunShin <doyun.shin@gmail.com>
2026-05-05 22:07:42 +0900

    chore(release): bump version to 1.0.0rc4

--- mirror/__init__.py
+++ mirror/__init__.py
@@ -12,7 +12,7 @@ worker: dict
 status: dict
 debug: bool = False
 exit: bool = False
-__version__ = "1.0.0-pre3"
+__version__ = "1.0.0rc4"
 
 STATE_PATH = Path("/var/lib/mirror/")
 RUN_PATH = Path("/var/run/mirror/")
