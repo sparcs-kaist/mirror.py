67315b8182e72ac36c225bce283e24d30c5b1f95
DoyunShin <doyun.shin@gmail.com>
2026-05-31 04:31:50 +0900

    chore: Version 1.1.1

--- mirror/__init__.py
+++ mirror/__init__.py
@@ -12,7 +12,7 @@ worker: dict
 status: dict
 debug: bool = False
 exit: bool = False
-__version__ = "1.1.1rc1"
+__version__ = "1.1.1"
 
 STATE_PATH = Path("/var/lib/mirror/")
 RUN_PATH = Path("/var/run/mirror/")
